e7faa75657ba82d64f24813ff12175442095f7ca
Vinay <Vinay@LAPTOP-PSE23HB1>
2024-09-26 12:35:12 +0530

    Netra

---
+++ framework/src/framework/array.java
@@ -0,0 +1,34 @@
+package framework;
+
+public class array {
+
+		public static void main(String[] args) 
+		{
+			int []msd= {2,3,4,5,6,6};
+			for(int i=msd.length-1;i>=0;i--)
+			{
+				System.out.println(msd[i]);
+			}
+
+			System.out.println("=========");
+			int []arr= {2,3,4,5,6,6};
+			for(int i=arr.length-1;i>=0;i--)
+			{
+				if(arr[i]%2==1)
+				{
+				System.out.println(msd[i]);
+				//System.out.println("=====");
+				}
+				else
+				{
+					System.out.println(msd[i]);
+				}
+				
+				
+				
+				
+		// TODO Auto-generated method stub
+
+	}
+
+}
